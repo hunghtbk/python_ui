a286d4e75d8181cf0b89835918183a3491a05ddc
hunght <hunght.thaibinh@gmail.com>
2021-07-24 23:10:30 +0700

    hide title bar

--- Class/ControllMainAPP.cpp
+++ Class/ControllMainAPP.cpp
@@ -7,7 +7,7 @@ void ControllMainAPP::initControlMainApp()
     m_uiBridge = new UIBridge(this);
     m_qmlEngine.rootContext()->setContextProperty("UIBridge", m_uiBridge);
 
-    m_qmlEngine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    m_qmlEngine.load(QUrl(QStringLiteral("qrc:/qmlScreen/loginScreen.qml")));
 }
 
 ControllMainAPP *ControllMainAPP::getInstance()
